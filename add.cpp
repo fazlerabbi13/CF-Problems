6d2be6985abc8c5d491c300f2a075fa4fcdd9072
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-16 23:31:20 +0600

    add two integer using function.

---
+++ add.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+ int add(int a,int b){
+        return a+b;
+    }
+
+int main(){
+    long long int a,b;
+    cin>>a>>b;
+
+    int sum= add(a,b);
+
+    cout<<sum;
+   
+    return 0;
+}
