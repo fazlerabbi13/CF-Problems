68554a14661a5fb39d49137aa88f9694892c4e40
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-24 20:07:55 +0600

    You are given an integer x. You need to find the smallest non-negative integer y such that the numbers x and y share at least one common digit. In other words, there must exist a decimal digit d that appears in both the representation of the number x and the number .

---
+++ Random-Problem/OnlyOneDigit.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+        string x;
+        cin>>x;
+
+        if(x.find('0') != string::npos){
+            cout<<0<<"\n";
+        }else{
+            char min_disit = *min_element(x.begin(),x.end());
+            // cout<<(min_disit-'0')<<"\n";
+            cout<<min_disit<<"\n";
+        }
+    }
+    
+    return 0;
+}
