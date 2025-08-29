a3b26eee253bb2a905b56d407447b454aaf59208
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-29 15:48:26 +0600

    Given the numbers of the two brothers who arrived on time, i  need to determine the number of the brother who was late.

---
+++ Random-Problem/ThreeBrothers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = 1; i <= 3; i++) {
+        if (i != a && i != b) {
+            cout << i << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
