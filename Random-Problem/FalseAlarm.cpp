8004d545c1d1f78754553d992de02758c67e2d4c
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-01 10:54:42 +0600

    taken test case.

---
+++ Random-Problem/FalseAlarm.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    int count = 0;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+
+        vector<int> door;
+        for(int i = 0; i<n; i++){
+            cin>>door[i];
+        }
+        for(int )
+    }
+    return 0;
+}
