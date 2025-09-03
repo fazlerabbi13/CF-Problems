c63327abec180451c9ce988158403b0ef069ca5e
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-03 15:40:17 +0600

    First half: even numbers.

--- Virtual-Contest/BalancedArray.cpp
+++ Virtual-Contest/BalancedArray.cpp
@@ -11,7 +11,6 @@ int main() {
         int n;
         cin >> n;
 
-        // Check if solution is possible
         if (n % 4 != 0) {
             cout << "NO\n";
             continue;
@@ -19,19 +18,26 @@ int main() {
 
         cout << "YES\n";
         int k = n / 2;
+        vector<int> arr;
 
         // First half: even numbers
         for (int i = 1; i <= k; i++) {
-            cout << 2 * i << " ";
+            arr.push_back(2 * i);
         }
 
-        // Second half: first k-1 odd numbers
-        for (int i = 1; i < k; i++) {
-            cout << 2 * i - 1 << " ";
-        }
+        // Second half: first (k-1) odd numbers
+        // for (int i = 1; i < k; i++) {
+        //     arr.push_back(2 * i - 1);
+        // }
 
         // Last odd number to balance sums
-        cout << 3 * k - 1 << "\n";
+        // arr.push_back(3 * k - 1);
+
+        // Print the vector
+        // for (int x : arr) {
+        //     cout << x << " ";
+        // }
+        // cout << "\n";
     }
     return 0;
 }
