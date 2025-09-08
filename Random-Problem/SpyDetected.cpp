2623a67a28e52ab89dc7c6cafec04dbf1b4567de
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-08 14:08:45 +0600

    Find the unique element.

--- Random-Problem/SpyDetected.cpp
+++ Random-Problem/SpyDetected.cpp
@@ -24,12 +24,12 @@ int main() {
         }
 
         // Step 2: Find the unique element
-        // for (int i = 0; i < n; i++) {
-        //     if (a[i] != majority) {
-        //         cout << (i + 1) << "\n";  // 1-based index
-        //         break;
-        //     }
-        // }
+        for (int i = 0; i < n; i++) {
+            if (a[i] != majority) {
+                cout << (i + 1) << "\n";  // 1-based index
+                break;
+            }
+        }
     }
 
     return 0;
