6b3888afb06f2c0197c869755f37b34f50659037
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-08 14:08:06 +0600

    Determine the majority value using first 3 elements.

---
+++ Random-Problem/SpyDetected.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        // Step 1: Determine the majority value using first 3 elements
+        int majority;
+        if (a[0] == a[1] || a[0] == a[2]) {
+            majority = a[0];
+        } else {
+            majority = a[1];
+        }
+
+        // Step 2: Find the unique element
+        // for (int i = 0; i < n; i++) {
+        //     if (a[i] != majority) {
+        //         cout << (i + 1) << "\n";  // 1-based index
+        //         break;
+        //     }
+        // }
+    }
+
+    return 0;
+}
--- Random-Problem/tempCodeRunnerFile.cpp
+++ Random-Problem/tempCodeRunnerFile.cpp
@@ -1,5 +1,6 @@
- // if (last - first + 1 <= x) {
-        //     cout << "YES\n";
-        // } else {
-        //     cout << "NO\n";
+for (int i = 0; i < n; i++) {
+        //     if (a[i] != majority) {
+        //         cout << (i + 1) << "\n";  // 1-based index
+        //         break;
+        //     }
         // }
