b3cd0b5bc63aa2bcf3bb47f01f8c7fc02193f3f6
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-31 11:52:28 +0600

    take input array and set value in prefix array.

--- Div4/OddQueries.cpp
+++ Div4/OddQueries.cpp
@@ -1,8 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define ll long long;
-
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -15,10 +13,13 @@ int main(){
         int n, q;
         cin >> n >> q;
 
-        vector<ll> arr(n+1);
-        vector<ll> prefixArr(n+1,0);
+        vector<long long> arr(n+1);
+        vector<long long> prefixArr(n + 1, 0);
 
-        
+        for(int i = 1; i <= n; ++i){
+            cin >> arr[i];
+            prefixArr[i] = prefixArr[i - 1] + arr[i];
+        }
 
 
     }
