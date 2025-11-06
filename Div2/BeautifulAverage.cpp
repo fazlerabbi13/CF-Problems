d9648026898b7a1bc9797da6c08c4474b4553fb3
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-06 19:41:03 +0600

    finding the maximum possible average value of any subarray∗ of the array a.

--- Div2/BeautifulAverage.cpp
+++ Div2/BeautifulAverage.cpp
@@ -1,13 +1,13 @@
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int t; cin >> t;
-//     while(t--){
-//         int n; cin >> n;
-//         vector<int> arr(n);
-//         for(int& i : arr) cin >> i;
-//         cout << *max_element(arr.begin(), arr.end()) << "\n";
-//     }
-//     return 0;
-// }
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        vector<int> arr(n);
+        for(int& i : arr) cin >> i;
+        cout << *max_element(arr.begin(), arr.end()) << "\n";
+    }
+    return 0;
+}
